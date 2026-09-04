17e42aa74ff71a2617fcf16a0f318bb697f9e2cd
Adarsha B M <adarsha23961@gmail.com>
2026-09-04 15:55:12 +0530

    Update #LCM of two numbers.py

--- #LCM of two numbers.py
+++ #LCM of two numbers.py
@@ -3,8 +3,7 @@
 #in Mathematics , Lcm of any two number its  
 #the value which is evenly divsible
 # by the two  gen number      
-
-
+  
 x = int(input("enter number >> "))    
 y = int(input("enter number >> "))
   
