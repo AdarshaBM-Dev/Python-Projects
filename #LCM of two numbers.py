86a3325ddd126d0fba45f8604c17b54aa3fb4f0c
Adarsha B M <adarsha23961@gmail.com>
2026-09-04 15:55:23 +0530

    Fix formatting and clean up LCM calculation script

--- #LCM of two numbers.py
+++ #LCM of two numbers.py
@@ -1,7 +1,7 @@
 #LCM of two numbers
 #LCM - Least common Multiple 
 #in Mathematics , Lcm of any two number its  
-#the value which is evenly divsible
+#the value which is evenly divsible 
 # by the two  gen number      
   
 x = int(input("enter number >> "))    
